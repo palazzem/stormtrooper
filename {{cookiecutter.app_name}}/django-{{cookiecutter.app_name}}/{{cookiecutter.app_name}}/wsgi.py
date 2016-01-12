eebd0935d918b8bbd9f08867766d13f37caea3ca
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-01-12 09:57:10 +0100

    removing Django template comments

--- {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/wsgi.py
+++ {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for {{ cookiecutter.app_name }} project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.app_name }}.settings")
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.app_name }}.settings")
 application = get_wsgi_application()
