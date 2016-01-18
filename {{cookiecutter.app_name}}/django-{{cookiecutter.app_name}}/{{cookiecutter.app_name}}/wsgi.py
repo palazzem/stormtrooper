ad59b20e5517280eb9de7d1c1c7cfd00ca138bef
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-01-18 14:28:37 +0100

    removing default Django settings module on wsgi file

--- {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/wsgi.py
+++ {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/wsgi.py
@@ -1,7 +1,4 @@
-import os
-
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.app_name }}.settings")
 application = get_wsgi_application()
