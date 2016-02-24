37ed45cbd06e48d2c16faa0353f747ed3e565afa
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-02-24 17:23:35 +0100

    using the right assertion in test_simple.py

--- {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/tests/simple/test_simple.py
+++ {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/tests/simple/test_simple.py
@@ -3,11 +3,11 @@ def test_simple_empty_test():
     This test is used as a placeholder so you can immediately
     test your CI integration
     """
-    self.assertEqual(1, 1)
+    assert 1 == 1
 
 def test_simple_empty_test_failure():
     """
     This test is used as a placeholder so you can immediately
     test your CI integration
     """
-    self.assertEqual(0, 1)
+    assert 0 == 1
