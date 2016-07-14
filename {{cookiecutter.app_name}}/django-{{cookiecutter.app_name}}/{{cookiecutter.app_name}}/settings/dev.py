950748a174cbc3e96cce7b2170d3246a57447eb5
Emanuele Palazzetti <hello@palazzetti.me>
2016-07-14 17:08:45 +0200

    fixed #11: add INTERNAL_IPS to dev settings

--- {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/settings/dev.py
+++ {{cookiecutter.app_name}}/django-{{cookiecutter.app_name}}/{{cookiecutter.app_name}}/settings/dev.py
@@ -4,6 +4,9 @@ from .base import *
 # removing security enforcement in development mode
 DEBUG = True
 SECRET_KEY = env('DJANGO_SECRET_KEY', '1234567890')
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
 
 # enabling console loggers
 LOGGING['loggers'] = {
